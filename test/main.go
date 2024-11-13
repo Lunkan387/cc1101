8167f633e31493d888ffd398b20a005bc8eada04
Lunkan387 <lunnkannn@gmail.com>
2024-11-13 23:02:05 +0100

    Marcstate erros

--- test/main.go
+++ test/main.go
@@ -25,7 +25,7 @@ func main() {
 	SPI2_CS_PIN.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	spi := machine.SPI2
 	err := spi.Configure(machine.SPIConfig{
-		Frequency: 1_000_000,
+		Frequency: 500_000,
 		SCK:       SCK,
 		SDO:       MOSI,
 		SDI:       MISO,
@@ -35,15 +35,16 @@ func main() {
 		panic(err)
 	}
 	cc := cc1101.New(spi, SPI2_CS_PIN.Set, SPI2_MISO_PIN)
+
 	if err := cc.Reset(); err != nil {
 		panic(err)
 	}
+	cc.SetFrequency(443.92)
 	time.Sleep(1 * time.Second)
-	fmt.Println(cc.ReadSingleRegister(cc1101.CC1101_MDMCFG2))
+	cc.SetRx()
+	fmt.Println()
+	marcState, _ := cc.ReadSingleRegister(cc1101.MARCSTATE)
+	fmt.Printf("MARCSTATE: %08b\n", marcState)
+	fmt.Println(cc.ReadBurstRegister(cc1101.FREQ2, 3))
 
-	cc.SetFrequency(433.92)
-	for {
-		fmt.Println(cc.ReadBurstRegister(cc1101.CC1101_FREQ2, 3))
-		time.Sleep(1 * time.Second)
-	}
 }
